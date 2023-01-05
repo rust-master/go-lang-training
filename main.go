00ef2792aeae0bec7797fce12027a43cf1e306ff
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2023-01-05 11:17:01 +0500

    arraysAndSlices

--- main.go
+++ main.go
@@ -11,8 +11,32 @@ func main() {
 	fmt.Println(quote.Go())
 
 	// variablesStringNums()
-	printingFormattingStrings()
+	// printingFormattingStrings()
+	arraysAndSlices()
 }
+
+func arraysAndSlices() {
+	var ages [7]int = [7]int{1,3,4}
+
+	var years = [5]int{1,3,4}
+
+	month := [6]string{"jan","feb","mar"}
+	month[3] = "apr"
+
+	fmt.Println(ages, len(ages))
+	fmt.Println(years)
+	fmt.Println(month)
+
+	// slices (use arrays under the hood)
+	var scores = []int{100, 40, 90}
+	scores[1] = 30
+	scores = append(scores, 85)
+	fmt.Println(scores, len(scores))
+
+	// slices ranges
+	rangeOne := scores[1:3] // not include the position 3
+	fmt.Println(rangeOne, len(rangeOne))
+ }
 
 func printingFormattingStrings() {
 	age := 20
