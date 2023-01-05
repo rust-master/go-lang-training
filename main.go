81c143af423530c28496970b29247a8fd6c0d600
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2023-01-05 10:58:27 +0500

    printingFormattingStrings

--- main.go
+++ main.go
@@ -10,7 +10,28 @@ func main() {
 	fmt.Println("Hi, Go Lang")
 	fmt.Println(quote.Go())
 
-	variablesStringNums()
+	// variablesStringNums()
+	printingFormattingStrings()
+}
+
+func printingFormattingStrings() {
+	age := 20
+	name := "Zaryab"
+	// Print: no new line
+	fmt.Print("Hello, ")
+	fmt.Print("Go ")
+	fmt.Print("Lang \n")
+
+	// with new line
+	fmt.Println("Hi Go") 
+	fmt.Println(name, "age is", age)
+
+	// Printf (formatted String) %_ = format specifeir
+	fmt.Printf("my age is %v and my name is %v", age, name)
+
+	// Sprintf (save formatted String)
+	var str = fmt.Sprintf("my age is %v and my name is %v", age, name)
+	fmt.Println("\nthe save string is:", str)
 }
 
 func variablesStringNums() {
