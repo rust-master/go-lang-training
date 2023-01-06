58f2d01c6b2cab8bdc821ebf858bb471997d1a01
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2023-01-06 18:39:38 +0500

    loops in go

--- main.go
+++ main.go
@@ -15,7 +15,33 @@ func main() {
 	// variablesStringNums()
 	// printingFormattingStrings()
 	// arraysAndSlices()
-	standardLibrary()
+	// standardLibrary()
+	loops()
+}
+
+func loops() {
+	x := 0
+
+	for x < 5 {
+		fmt.Println("value of x is:", x)
+		x++
+	}
+
+	for i := 0; i<5; i++ {
+		fmt.Println("value of i is:", i)
+	}
+
+	// loop throught slice
+	coins := []string{"ETH", "BTC", "DOT", "ATOM", "SOL", "USDT", "USDC", "BNB", "AVAX"}
+
+	for i :=0; i < len(coins); i++ {
+		fmt.Println(coins[i])
+	}
+
+	// for in loop
+	for index, value := range coins {
+		fmt.Printf("%v at the position %v\n", value, index)
+	}
 }
 
 func standardLibrary() {
