986ae81025d9624e9a2640a53ae3039eea8c046e
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2023-01-06 13:15:27 +0500

    standard library in go

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 
 	"rsc.io/quote"
 )
@@ -12,7 +14,24 @@ func main() {
 
 	// variablesStringNums()
 	// printingFormattingStrings()
-	arraysAndSlices()
+	// arraysAndSlices()
+	standardLibrary()
+}
+
+func standardLibrary() {
+	greeting := "Hello there Go Devs"
+
+	fmt.Println(strings.Contains(greeting, "Hi"))
+	fmt.Println(strings.ReplaceAll(greeting, "Hello","Hi"))
+	fmt.Println(strings.Index(greeting, "Go"))
+
+	// sort
+	age := []int{54,43,23,53,56,11,8,99,111}
+	sort.Ints(age)
+	fmt.Println(age)
+
+	index := sort.SearchInts(age, 23)
+	fmt.Println(index)
 }
 
 func arraysAndSlices() {
